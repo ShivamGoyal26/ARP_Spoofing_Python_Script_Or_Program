375ebd73b9222829f256a917717af637554a7245
shivamgoyal26 <officialshivamgoyal@gmail.com>
2020-01-16 17:04:08 -0500

    1st commit

---
+++ ARP_Spoofing.py
@@ -0,0 +1,10 @@
+# Now what is the ARP Spoofing, this is similar to the man in the middle attack here bascially
+# All the information of the client like what he is seraching,watching,entering passwords etc
+# all these details will pass through our machine then we can drop, modify etc with that
+#     information after this info will go to the router and then so on ....
+#
+# and when the server respond then router will give the response to us then similary
+# we can modify,drop etc with the respone and then pass it to the client
+# 
+# basically here we make fool to router that i am the client
+# and to client that i am the router
